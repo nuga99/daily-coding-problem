ea61e6483ca1c9913908bb255603988bcd614dd2
Muhammad Ardivan Satrio Nugroho <nugamuhammad@gmail.com>
2019-02-02 22:18:13 +0700

    rename problems

---
+++ Problem 13/solve.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+'''
+Problems #13
+
+Good morning! Here's your coding interview problem for today.
+
+This problem was asked by Amazon.
+
+Given an integer k and a string s, find the length of the longest substring that contains at most k distinct characters.
+
+For example, given s = "abcba" and k = 2, the longest substring with k distinct characters is "bcb".
+
+Upgrade to premium and get in-depth solutions to every problem. Since you were referred by one of our affiliates, you'll get a 10% discount on checkout!
+
+If you liked this problem, feel free to forward it along so they can subscribe here! As always, shoot us an email if there's anything we can help with!
+'''
+
+def solve(s,k):
+    print(s,k)
+
+
+if __name__ == "__main__":
+    s = input()
+    k = int(input())
+    solve(s,k)
